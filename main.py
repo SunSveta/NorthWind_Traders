1bf1b51e8144a34e278d71c4c7aca0466ccd49bf
yanochkina_sl <yanochkinasvetlana@mail.ru>
2023-01-16 12:57:31 +0600

    Fill in table suppliers, fill in column in table products

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import json
 
 conn = psycopg2.connect(host='localhost', database='northwind_traders', user='postgres', password='UNloCKed72')
 with conn.cursor() as cur:
+    result_products_lst = {}
     with open('suppliers.json') as r_file:
         data = json.load(r_file)
         for i in data:
-            cur.execute("INSERT INTO suppliers(company_name, contact_name, country, region, city, phone) VALUES (%s, %s, %s, %s, %s, %s)",
+            cur.execute("INSERT INTO suppliers(company_name, contact_name, country, region, city, phone) VALUES (%s, %s, %s, %s, %s, %s) RETURNING supplier_id",
                         (i['company_name'],
                          i['contact'].split(', ')[0],
                          i['address'].split('; ')[0],
@@ -14,6 +15,18 @@ with conn.cursor() as cur:
                          i['address'].split('; ')[3],
                          i['phone'])
                         )
-            conn.commit()
+            sup_id = cur.fetchone()[0]
+
+            for j in i.get('products'):
+                result_products_lst.update({j:sup_id})
+        #print(result_products_lst) #каждый раз продолжает ид, ???
+        big_sql_row = ''
+        for k, v in result_products_lst.items():
+            prod_name = k
+            name = prod_name.replace("'", "''")
+            big_sql_row += f"UPDATE products SET supplier_id = {v} WHERE product_name = '{name}'; "
+        cur.execute(big_sql_row)
+        conn.commit()
+
 
 
